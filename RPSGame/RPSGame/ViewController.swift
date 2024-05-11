60b7e51b309c88257c82c7ba22dff8998f5768f5
won <andrew112517@naver.com>
2024-05-11 20:12:47 +0900

    [Feat]버튼 함수 세팅

--- RPSGame/RPSGame/ViewController.swift
+++ RPSGame/RPSGame/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Variables
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 

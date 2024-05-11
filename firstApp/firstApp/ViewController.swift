5ada5ede163725d1b7660048091954d6a2a253dd
won <andrew112517@naver.com>
2024-05-11 17:17:15 +0900

    [Feat]코드 추가

--- firstApp/firstApp/ViewController.swift
+++ firstApp/firstApp/ViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        mainLabel.text = "안녕하세요"
+        mainLabel.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        mainLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+    }
+    
 
 
 }

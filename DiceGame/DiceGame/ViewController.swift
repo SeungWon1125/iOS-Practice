d568701d24d53ec02ff5af2ad4e050a563200352
won <andrew112517@naver.com>
2024-05-11 19:36:39 +0900

    [Feat]뷰 구성

---
+++ DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 김승원 on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

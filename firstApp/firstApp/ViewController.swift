9d342b88bc48dcd6c11abc4910d22fc2cadcf4f7
won <andrew112517@naver.com>
2024-05-11 17:30:03 +0900

    [Feat]button IBOutlet추가

--- firstApp/firstApp/ViewController.swift
+++ firstApp/firstApp/ViewController.swift
@@ -11,17 +11,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
     
+    @IBOutlet weak var myButton: UIButton!
+    
+    
+    // 앱의 화면에 들어오면 처음 실행시키는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         mainLabel.text = "안녕하세요"
         mainLabel.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        mainLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        mainLabel.textColor = .yellow
         
+        myButton.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        myButton.setTitleColor(.yellow, for: .normal)
     }
     
 

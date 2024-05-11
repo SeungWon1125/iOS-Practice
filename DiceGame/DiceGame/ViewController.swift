601a1f14bf2ed1cf0edeef1840ffa41daf299be8
won <andrew112517@naver.com>
2024-05-11 19:51:56 +0900

    [Feat]랜덤 주사위 코드 추가

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Variables
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstImageView.image = diceArray.randomElement()
+        secondImageView.image = diceArray.randomElement()
+        
     }
 
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+        // 첫 번째 이미지뷰의 이미지를 랜덤으로 변경
+        firstImageView.image = diceArray.randomElement()
+        
+        // 두 번째 이미지뷰의 이미지를 랜덤으로 변경
+        secondImageView.image = diceArray.randomElement()
+        
+    }
+    
 
 }
 

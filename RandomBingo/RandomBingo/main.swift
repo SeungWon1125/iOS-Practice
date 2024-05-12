5a55fa8859f31f03e2ae2057d337e6c70b492288
won <andrew112517@naver.com>
2024-05-12 22:58:30 +0900

    [Feat]RandomBingo 완성

--- RandomBingo/RandomBingo/main.swift
+++ RandomBingo/RandomBingo/main.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 
+var computerChoice = Int.random(in: 1...100)
+var myChoice: Int = 0
+
+while true {
+    
+    var userInput = readLine()
+    
+    if let input = userInput {
+        if let number = Int(input) {
+            myChoice = number
+        }
+    }
+    
+    if computerChoice > myChoice {
+        print("up")
+    } else if computerChoice < myChoice {
+        print("down")
+    } else {
+        print("Bingo")
+        break
+    }
+}

2b594a5302cda063b9f88aa2453d1bc582a68c38
won <andrew112517@naver.com>
2024-05-14 19:04:50 +0900

    [Feat]첫번째 앱 프로젝트 세팅

---
+++ firstNewApp/firstNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstNewApp
+//
+//  Created by 김승원 on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

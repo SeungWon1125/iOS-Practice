033018796e8c15b3a03e59ba357e2ee1dc7b65d3
won <andrew112517@naver.com>
2024-05-14 19:42:10 +0900

    [Feat]첫번째 앱 슬라이더 초기값 세팅

--- firstNewApp/firstNewApp/ViewController.swift
+++ firstNewApp/firstNewApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mainLabel: UILabel!
     
+    @IBOutlet weak var slider: UISlider!
+    
     // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,18 +24,32 @@ class ViewController: UIViewController {
     // MARK: - configureUI
     func configureUI() {
         mainLabel.text = "초를 선택하세요"
+        
+        // 슬라이더 가운데 설정
+        slider.value = 0.0
+        
+        
     }
     
     // MARK: - Actions
     @IBAction func sliderChanged(_ sender: UISlider) {
+        // 슬라이더 벨류값을 가지고 메인 레이블의 텍스트 세팅
+        let seconds = String(Int(slider.value * 60))
+        mainLabel.text = seconds + " sec"
         
     }
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
+        // 1초씩 지나갈 때마다 무언가를 실행
         
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
+        // 초기화 세팅
+        mainLabel.text = "초를 선택하세요"
+
+        // 슬라이더 가운데 설정
+        slider.setValue(0.0, animated: true)
         
     }
     

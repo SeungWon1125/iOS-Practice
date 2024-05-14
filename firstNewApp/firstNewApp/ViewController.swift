101cd95795a1b4d895733d9f3da22fe2456704ab
won <andrew112517@naver.com>
2024-05-14 19:23:42 +0900

    [Feat]첫번째 앱 코드 세팅

--- firstNewApp/firstNewApp/ViewController.swift
+++ firstNewApp/firstNewApp/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Variables
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        
     }
-
-
+    
+    // MARK: - configureUI
+    func configureUI() {
+        mainLabel.text = "초를 선택하세요"
+    }
+    
+    // MARK: - Actions
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 

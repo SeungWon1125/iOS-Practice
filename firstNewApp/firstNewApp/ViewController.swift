a0b562a57bfe60e311d2547a2dbb426620f385c1
won <andrew112517@naver.com>
2024-05-14 21:21:36 +0900

    [Feat]첫번째 앱 타이머 코드 selector 사용

--- firstNewApp/firstNewApp/ViewController.swift
+++ firstNewApp/firstNewApp/ViewController.swift
@@ -48,25 +48,46 @@ class ViewController: UIViewController {
     @IBAction func startButtonTapped(_ sender: UIButton) {
         // 1초씩 지나갈 때마다 무언가를 실행
         timer?.invalidate() // 혹시 몰라 타이머를 미리 죽이는 코드
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self]_ in
-            //  반복하고 싶은 코드
-            if number > 0 {
-                number -= 1
-                slider.value = Float(number) / Float(60) // value에 0~1사이의 Float값만 들어갈 수 있음
-                mainLabel.text = "\(number) sec"
-                
-            } else {
-                number = 0
-                mainLabel.text = "초를 선택하세요"
-                // 소리를 나게 해야함
-                AudioServicesPlayAlertSound(SystemSoundID(1322))
-                
-                // 타이머 설정 죽어여야 함
-                timer?.invalidate() // 타이머 죽이는 코드
-            }
-            
-        }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(doSomthingAfter1Second), userInfo: nil, repeats: true)
         
+//        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self]_ in
+//            //  반복하고 싶은 코드
+//            if number > 0 {
+//                number -= 1
+//                slider.value = Float(number) / Float(60) // value에 0~1사이의 Float값만 들어갈 수 있음
+//                mainLabel.text = "\(number) sec"
+//                
+//            } else {
+//                number = 0
+//                mainLabel.text = "초를 선택하세요"
+//                // 소리를 나게 해야함
+//                AudioServicesPlayAlertSound(SystemSoundID(1322))
+//                
+//                // 타이머 설정 죽어여야 함
+//                timer?.invalidate() // 타이머 죽이는 코드
+//            }
+//            
+//        }
+        
+    }
+    
+    @objc func doSomthingAfter1Second() {
+        if number > 0 {
+            number -= 1
+            slider.value = Float(number) / Float(60) // value에 0~1사이의 Float값만 들어갈 수 있음
+            mainLabel.text = "\(number) sec"
+            
+        } else {
+            number = 0
+            mainLabel.text = "초를 선택하세요"
+            // 소리를 나게 해야함
+            AudioServicesPlayAlertSound(SystemSoundID(1322))
+            
+            // 타이머 설정 죽어여야 함
+            timer?.invalidate() // 타이머 죽이는 코드
+            // 타이머는 스스로 언제 끝날지 모르기 때문에
+            // 타이머를 죽이는 코드를 꼭 만들어야 함
+        }
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {

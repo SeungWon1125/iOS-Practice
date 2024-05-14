a2a7ad48a1cff057bdf5b46c2d4df57b905610b4
won <andrew112517@naver.com>
2024-05-14 21:38:21 +0900

    [Feat]두번째 앱 프로젝트, UI세팅

---
+++ TextFieldProject/TextFieldProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextFieldProject
+//
+//  Created by 김승원 on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

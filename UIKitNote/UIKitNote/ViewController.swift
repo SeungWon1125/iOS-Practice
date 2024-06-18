d02ed28a03a7f8f8ce0188e279d9ae9189ddaa00
won <andrew112517@naver.com>
2024-06-18 14:52:13 +0900

    [Feat]UIKit note생성

---
+++ UIKitNote/UIKitNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitNote
+//
+//  Created by 김승원 on 6/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+

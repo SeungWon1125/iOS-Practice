feab611e093a3da3ea2f6eee26cccee91685403e
won <andrew112517@naver.com>
2024-06-19 20:35:23 +0900

    [Feat]코어데이터 프로젝트 설정

---
+++ ToDoApp/ToDoApp/Models/MemoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MemoData+CoreDataClass.swift
+//  ToDoApp
+//
+//  Created by 김승원 on 6/19/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MemoData)
+public class MemoData: NSManagedObject {
+
+}

55930bad2b5ae6ede0b4d054688034374d0d0ebd
won <andrew112517@naver.com>
2024-06-22 16:35:10 +0900

    [Feat]프로젝트 생성

---
+++ CollectionViewPractice/CollectionViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewPractice
+//
+//  Created by 김승원 on 6/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
